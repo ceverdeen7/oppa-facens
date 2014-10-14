e455c5be0dd62d40dc0ec04c7878402550bfa726
drexduarte@gmail.com <drexduarte@gmail.com@49249c83-ae39-7a03-5e5c-ab734e3c6abd>
2014-10-14 17:05:43 +0000

    Version 1.0 - First Commit - Fuzzy implemented - PSO initial version - Drawer implemented

---
+++ trunk/OPPA/PSO/PSOHandler.cs
@@ -0,0 +1,42 @@
+﻿using OPPA.Fuzzy;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPA.PSO
+{
+    public class PSOHandler
+    {
+        private List<Particle> swarm;
+
+        public PSOHandler(int particles, int moves)
+        {
+            GenerateSwarm(particles, moves);
+        }
+
+        public void UpdateSwarm()
+        {
+            Parallel.ForEach(swarm, p => {
+                FIS fis = new FIS();
+                while (p.HasMoves)
+                {
+                    p.Speed = fis.getSpeed(p.Speed, p.Acceleration, p.Brake);
+                    p.WheelAngle = fis.getWheelAngle(p.SteeringWheel);
+                    p.NextMove();
+                }
+            });
+        }
+
+        public void GenerateSwarm(int particles, int moves)
+        {
+            swarm = new List<Particle>(particles);
+            Parallel.For(0, particles, i =>
+            {
+                swarm.Add(new Particle(moves));
+            });
+        }
+    }
+}
---
+++ trunk/OPPA/PSO/Particle.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPA.PSO
+{
+    public class Particle
+    {
+        float[,] moves;
+        int steps, actual;
+
+        /// <summary>
+        /// List of particle's moves
+        /// moves[i,0] = Acceleration
+        /// moves[i,1] = Brake
+        /// moves[i,2] = SteeringWheel
+        /// moves[i,3] = Speed
+        /// moves[i,4] = WheelAngle 
+        /// </summary>
+        public float[,] Moves
+        {
+            get { return moves; }
+            set { moves = value; }
+        }
+
+        public float Acceleration
+        {
+            get { return moves[actual, 0]; }
+        }
+
+        public float Brake
+        {
+            get { return moves[actual, 1]; }
+        }
+
+
+        public float SteeringWheel
+        {
+            get { return moves[actual, 2]; }
+        }
+
+        /// <summary>
+        /// Actual speed of the car
+        /// </summary>
+        public float Speed
+        {
+            get { return moves[actual, 3]; }
+            set { moves[actual + 1, 3] = value; }
+        }
+
+        public float WheelAngle
+        {
+            get { return moves[actual, 4]; }
+            set { moves[actual + 1, 4] = value; }
+        }
+
+        public bool HasMoves
+        {
+            get { return !(actual == steps); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="steps">Number of particle's moves.</param>
+        public Particle(int steps)
+        {
+            this.steps = steps;
+            actual = 0;
+            moves = new float[steps+1, 5];
+            RandomMoves();
+        }
+
+        /// <summary>
+        /// Generate random particle's moves
+        /// </summary>
+        private void RandomMoves()
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            Parallel.For(0, steps, i =>
+            {
+                moves[i, 0] = (float)r.NextDouble();
+                moves[i, 1] = (float)r.NextDouble();
+                moves[i, 2] = (float)r.NextDouble()*5.0f - 2.5f;
+                moves[i, 3] = moves[i, 4] = 0;
+            });
+        }
+
+        public void NextMove()
+        {
+            if(HasMoves) actual++;
+        }
+    }
+}
