e85991c0e45c5a6e600cea47e6c48799cfbc8fcc
drexduarte@gmail.com <drexduarte@gmail.com@49249c83-ae39-7a03-5e5c-ab734e3c6abd>
2014-11-16 01:44:49 +0000

    frmSimulator - changing checkpoint; OPPA.csproj - changing AForge.Fuzzy reference and adding GA classes; PSOHandler - Adding Neighborhood concept and new method to export CSV file; WorldController - Adding GA concept; Particle - Adding neighborhood concept, changing Evaluate method, changing particle's initial values; Adding GA classes and Neighborhood classes.

---
+++ branches/geneticos/OPPA/PSO/Neighborhood/RRingNeighborhood.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPA.PSO.Neighborhood
+{
+    public class RRingNeighborhood : INeighborhood
+    {
+        public void Arrange(List<Particle> swarm)
+        {
+            int maxIndex = (swarm.Count - 1);
+            for (int i = 1; i < maxIndex; i++)
+            {
+                swarm[i].Neighbors.Add(swarm[i - 1]);
+                swarm[i].Neighbors.Add(swarm[i + 1]);
+                RandomNeighbor(swarm[i], swarm);
+            }
+            swarm[0].Neighbors.Add(swarm[maxIndex]);
+            swarm[0].Neighbors.Add(swarm[1]);
+            RandomNeighbor(swarm[0], swarm);
+            swarm[maxIndex].Neighbors.Add(swarm[maxIndex - 1]);
+            swarm[maxIndex].Neighbors.Add(swarm[0]);
+            RandomNeighbor(swarm[maxIndex], swarm);
+        }
+
+        private void RandomNeighbor(Particle p, List<Particle> swarm)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            Particle n = swarm[random.Next(swarm.Count)];
+            if (p != n && !p.Neighbors.Contains(n))
+            {
+                p.Neighbors.Add(n);
+                if (!n.Neighbors.Contains(p)) n.Neighbors.Add(p);
+            }
+        }
+    }
+}
